94b9c9c6b5c50d911e10e661e09fc836bbf2be1b
TruongQuocTrung <quoctrung13102000@gmail.com>
2021-09-01 23:26:21 +0700

    fix loi 2

--- src/BackgroundQL/QlyDiem.java
+++ src/BackgroundQL/QlyDiem.java
@@ -388,7 +388,7 @@ public class QlyDiem extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new QlyDiem().setVisible(true);
+                
             }
         });
     }
