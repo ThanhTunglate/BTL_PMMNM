4161cbb16914205ac95ca1507b48f454ca7cb6b9
TruongQuocTrung <quoctrung13102000@gmail.com>
2021-09-01 23:06:02 +0700

    Sua Diem 2

--- src/BackgroundQL/QlyDiem.java
+++ src/BackgroundQL/QlyDiem.java
@@ -20,10 +20,7 @@ public class QlyDiem extends javax.swing.JFrame {
     /**
      * Creates new form QlyDiem
      */
-    public String MaMH;
-    public String TenMH;
-    public String SoSV;
-    public QlyDiem() {
+    public QlyDiem(String MaMH, String TenMH, String SoSV) {
         initComponents();
         setLocationRelativeTo(null);
         txtMaMon.setText(MaMH);
@@ -34,7 +31,7 @@ public class QlyDiem extends javax.swing.JFrame {
     ArrayList<eDiem> list;
     DaoDiem dao = new DaoDiem();
     public void HienThi(){
-        list = dao.getDiem(MaMH);
+        list = dao.getDiem(txtMaMon.getText());
         tableQLDiem.setModel(new customQuanLyDiem(list));
     }
     /**
@@ -361,37 +358,6 @@ public class QlyDiem extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(QlyDiem.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(QlyDiem.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(QlyDiem.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(QlyDiem.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new QlyDiem().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnBack;
